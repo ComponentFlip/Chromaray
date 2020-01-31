da7740ec06da7f299284e4dc739fa3a12684fd53
h5p9sl <21267024+h5p9sl@users.noreply.github.com>
2020-01-31 14:16:07 -0700

    Add texture warp parameters

--- src/gfx/Texture.cpp
+++ src/gfx/Texture.cpp
@@ -11,6 +11,8 @@ Texture::Texture(const Image& image) {
 	// Set image scaling to nearest neighbour to keep the pixels sharp
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP);
 
 	unbind();
 }
